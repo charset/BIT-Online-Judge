7e82c127b092dcdd5453c64d02f0805fb0bfb57b
lytning98 <me@lytning.xyz>
2017-08-28 20:57:33 +0800

    比赛系统前端首页部分完成

---
+++ SiteUI/Models/ContestCatalog.cs
@@ -0,0 +1,13 @@
+﻿namespace BITOJ.SiteUI.Models
+{
+    /// <summary>
+    /// 编码比赛库目录类别。
+    /// </summary>
+    public enum ContestCatalog : int
+    {
+        All = 0,
+        Scheduled = 1,
+        Running = 2,
+        Ended = 3,
+    }
+}
